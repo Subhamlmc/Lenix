4f886bf87818498fae5d574c4e33b5c11e620930
Subham Lamichhane <lamichhanesubham4@gmail.com>
2025-06-10 18:37:47 -0700

    Update mac-spoofer.py
    Now unique macs would be selected that would no longer match with devices globally !!

--- mac-spoofer.py
+++ mac-spoofer.py
@@ -1,20 +1,19 @@
 import random
-import time
 import subprocess
-alphabet = "ABCDEF"
-digits = "0123456789"
-blender= alphabet + digits
+import time
+
+def generate_valid_mac():
+    first_byte = random.randint(0x00, 0xff)
+    first_byte = (first_byte & 0b11111110) | 0b00000010
+    mac = [first_byte] + [random.randint(0x00, 0xff) for _ in range(5)]
+    return ":".join(f"{x:02x}" for x in mac)
+
+interface = "wlan0" #select one which is in monitor mode like the adapter's one
 
 while True:
-    mixer = ""
-    for _ in range(6):
-        pair = random.choice(blender) + random.choice(blender)
-        mixer += pair + ":"
-    mixer = mixer.rstrip(":") 
-    time.sleep(2)
-    subprocess.run(["sudo","ifconfig","Wi-Fi","down"])
-    time.sleep(2)
-    subprocess.run(["sudo","ifconfig","Wi-Fi","hw","ether",mixer])
-    time.sleep(2)
-    subprocess.run(["sudo","ifconfig","Wi-Fi","up"])
-    time.sleep(15)
+    new_mac = generate_valid_mac()
+    print(f"Changing MAC to: {new_mac}")
+    subprocess.run(["sudo", "ifconfig", interface, "down"])
+    subprocess.run(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.run(["sudo", "ifconfig", interface, "up"])
+    time.sleep(10) 
